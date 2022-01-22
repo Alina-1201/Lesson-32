e95b8e9a72597e11bc1232ba6fccc91f8a694639
Alina-1201 <91902717+Alina-1201@users.noreply.github.com>
2022-01-22 13:43:11 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,156 @@
+// let n = prompt ('n = ')
+// if (n > 0){
+//     n++
+// }
+// alert(n)
+
+
+// let n = prompt ('n = ')
+// if (n > 0){
+//     n++
+// } else if (n < 0)  {
+//    n = n-2
+// }
+// alert (n)
+
+
+// let n = prompt ('n = ')
+// if (n > 0){
+//     n++
+// } else if (n < 0)  {
+//     n = n-2
+// } else if (n = 0) {
+//     n = n*1 + 10
+// }
+// alert (n)
+
+
+// let a = prompt ('a=')
+// let b = prompt ('b=')
+// let c = prompt ('c=')
+
+// if (a>0 , b>0 , c>0){
+//     m = ('Положительных чисел - 3')
+// } else if( a>0 , b>0 , c<0){
+//     m = ('Положительных чисел - 2')
+// } else if( a>0 , b<0 , c>0){
+//     m = ('Положительных чисел - 2')
+// } else if( a<0 , b>0 , c>0){
+//     m = ('Положительных чисел - 2')
+// } else if (a>0 , b<0 , c<0) {
+//     m = ('Положительных чисел - 1')
+// } else if (a<0 , b>0 , c<0) {
+//     m = ('Положительных чисел - 1')
+// } else if (a<0 , b<0 , c>0) {
+//     m = ('Положительных чисел - 1')
+// }
+// alert (m)
+
+
+// let a = prompt ('a=')
+// let b = prompt ('b=')
+// let c = prompt ('c=')
+
+// if (a>0 , b>0 , c>0){
+//     m = ('Положительных чисел - 3  Отрицательных чисел - 0')
+// } else if( a>0 , b>0 , c<0){
+//     m = ('Положительных чисел - 2  Отрицательных чисел - 1')
+// } else if( a>0 , b<0 , c>0){
+//     m = ('Положительных чисел - 2  Отрицательных чисел - 1')
+// } else if( a<0 , b>0 , c>0){
+//     m = ('Положительных чисел - 2  Отрицательных чисел - 1')
+// } else if (a>0 , b<0 , c<0){
+//     m = ('Положительных чисел - 1  Отрицательных чисел - 2')
+// } else if (a<0 , b>0 , c<0){
+//     m = ('Положительных чисел - 1  Отрицательных чисел - 2')
+// } else if (a<0 , b<0 , c>0){
+//     m = ('Положительных чисел - 1  Отрицательных чисел - 2')
+// } else if (a<0,b<0,c<0){
+//     m = ('Положительных чисел - 0  Отрицательных чисел - 3')
+// }
+// alert (m)
+
+
+// let a = prompt ('a=')
+// let b = prompt ('b=')
+
+// if (a>b){
+//     alert(a)
+// } else{
+//     alert(b)
+// }
+
+
+// let a = prompt ('a=')
+// let b = prompt ('b=')
+
+// if (a<b){
+//     alert(a)
+// } else{
+//     alert(b)
+// }
+
+
+// let a = prompt ('a=')
+// let b = prompt ('b=')
+
+// if (a>b){
+//     alert( a + ' ' + b)
+// } else {
+//     alert (b + ' ' + a)
+// }
+
+
+// let a = prompt ('a=')
+// let b = prompt ('b=')
+
+// if (a>b){
+//     alert(a + ' ' +b)
+// } else {
+//     alert (b + ' ' + a)
+// }
+
+
+
+// №11
+
+// let a = prompt ('a=')
+// let b = prompt ('b=')
+
+// if (a=b){
+//     alert ('Null')
+// } else {
+//     m = a*1+b*1
+// }
+// alert (m)
+
+
+// let a = prompt ('a=')
+// let b = prompt ('b=')
+// let c = prompt ('c=')
+
+// if (a<b , a<c){
+//     alert (a)
+// } else if (b<a , b<c){
+//     alert (b)
+// } else if (c<b , c<a){
+//     alert (c)
+// }
+
+
+
+// №13
+
+// let a = prompt ('a=')
+// let b = prompt ('b=')
+// let c = prompt ('c=')
+
+// if (c<a , a<b , b<a , a<c){
+//     alert (a)
+// } else if (c<b , b<a , a<b , b<c){
+//     alert (b)
+// } else if (a<c , c<b , b<c , c<a) {
+//     alert (c)
+// }
+
+
